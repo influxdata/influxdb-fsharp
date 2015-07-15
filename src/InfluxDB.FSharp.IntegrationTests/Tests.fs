ecf5e63c25a2e6879692baff451683bb35b579f9
pmartynov <pavel.martynov@kaspersky.com>
2015-07-15 14:54:20 +0300

    Integration test on Ping with real InfluxDB 0.9.1 inside Vagrant

---
+++ src/InfluxDB.FSharp.IntegrationTests/Tests.fs
@@ -0,0 +1,17 @@
+﻿[<NUnit.Framework.TestFixture>]
+module InfluxDB.FSharp.IntegrationTests.Tests
+
+open NUnit.Framework
+open InfluxDB.FSharp
+
+let (=?) (actual: 'a) (expected: 'a) =
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+let ``Ping``() =
+    let client = Client("localhost")
+    match client.Ping() with
+    | Choice1Of2 (elapsed, version) ->
+        printfn "ping elapsed: %O, version: %s" elapsed version
+        version =? "0.9.1"
+    | Choice2Of2 err -> failwithf "errror: %A" err
